6e03a2bde0be64b6a93ec4ff8bb9185be79ce077
Jirlan Souza <jirlansouza08@gmail.com>
2021-09-17 22:15:02 -0400

    Changed the sideBar and Content to separeited component

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,35 @@
-export function SideBar() {
-  // Complete aqui
+import { useState } from "react";
+import { Button } from "./Button";
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface SideBarProps {
+  genres: GenreResponseProps[],
+  selectedGenreId: number,
+  getSelectedGenreId: (id: number) => void
+}
+
+export function SideBar(props: SideBarProps) {
+  return (
+    <nav className="sidebar">
+        <span>Watch<p>Me</p></span>
+
+        <div className="buttons-container">
+          {props.genres.map(genre => (
+            <Button
+              key={String(genre.id)}
+              title={genre.title}
+              iconName={genre.name}
+              onClick={() => props.getSelectedGenreId(genre.id)}
+              selected={props.selectedGenreId === genre.id}
+            />
+          ))}
+        </div>
+
+      </nav>
+  )
 }
